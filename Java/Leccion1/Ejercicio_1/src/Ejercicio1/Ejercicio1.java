516255a4ecf04c6d328a7a9d8f387b85bb4bdcba
Facundo Giuliano <facugiuliano10@gmail.com>
2022-05-30 12:17:21 -0300

    Guardamos Java

--- Java/Leccion1/Ejercicio_1/src/Ejercicio1/Ejercicio1.java
+++ Java/Leccion1/Ejercicio_1/src/Ejercicio1/Ejercicio1.java
@@ -20,8 +20,20 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        String entrada = "Ingrese los siguientes datos del libro: "
-        Scanner entrada = new Scanner(System.in );
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese los siguientes datos del libro: ");
+        System.out.println("Digite el nombre del libro:");
+        String nombre = entrada.nextLine();
+        System.out.println("Digite el ID del libro:");
+        int ID = Integer.parseInt(entrada.nextLine());
+        System.out.println("Digite el precio del libro: ");
+        double precio = Double.parseDouble(entrada.nextLine());
+        System.out.println("Indique si el envio es gratuito: ");
+        boolean envio = Boolean.parseBoolean(entrada.nextLine());
         
+        System.out.println("Nombre: " + nombre);
+        System.out.println("ID = " + ID);
+        System.out.println("Precio = " + precio);
+        System.out.println("Envio = " + envio);
     }
 }
