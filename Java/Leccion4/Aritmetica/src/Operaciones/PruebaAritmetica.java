cd430f1e888e6404ec8d1fb1ea637657546141a8
Facundo Giuliano <facugiuliano10@gmail.com>
2022-10-15 12:21:53 -0300

    Java leccion 2 y 5
    Ejercicios 11 y 12, encapsulamiento

--- Java/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -25,10 +25,47 @@ public class PruebaAritmetica {
         Aritmetica aritmetica2 = new Aritmetica(5, 8);
         System.out.println("aritmetica2 = " + aritmetica2.a);
         System.out.println("aritmetica2 = " + aritmetica2.b);
+        Persona persona = new Persona("Facundo", "Giuliano");
+        System.out.println("persona = " + persona);
+        System.out.println("Persona nombre: "+persona.nombre);
+        System.out.println("Persona apellido: "+persona.apellido);
     }
     
+    // Modularidad creamos un nuevo metodo
     public static void miMetodo() {
         // int a = 10; //Una variable esta limitada
         System.out.println("Aquie hay otro metodo");
     }
 }
+
+// Creamos una nueva clase
+class Persona {
+    String nombre;
+    String apellido;
+    
+    Persona(String nombre, String apellido) { //Constructor
+        //super(); //Llamada al contructor de la clase padre Object
+        //Imprimir imprimir = new Imprimir();
+        new Imprimir().imprimir(this);
+        this.nombre = nombre;
+        this.apellido = apellido;
+        System.out.println("Objeto persona usando this: "+this);
+    }
+}
+
+class Imprimir {
+    public Imprimir() {
+        super(); //El constructor de la clase padre para reservar memoria
+    }
+    
+    public void imprimir(Persona persona) {
+        System.out.println("Persona desde la clase Imprimir "+persona);
+        System.out.println("Impresion del objeto actual (this): "+this);
+    }
+}
+
+
+
+
+
+
