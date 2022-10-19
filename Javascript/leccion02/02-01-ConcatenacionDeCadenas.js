0a071dede53fe5d08b88d7bf8562ab18c677f917
Facundo Giuliano <facugiuliano10@gmail.com>
2022-10-19 15:37:08 -0300

    Concatenacion de cadenas

---
+++ Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,16 @@
+var nombre = "Jose ";
+var apellido = "Montes"
+var nombreCompleto = nombre+" "+apellido;
+console.log(nombreCompleto);
+var nombreCompleto2 = "Facundo"+" "+"Giuliano";
+console.log(nombreCompleto2);
+// Lee de izq a der. Se pueden usar parentesis
+var juntos = nombre + (219 + 1);
+console.log(juntos);
+juntos = 78 + 17 + nombre;
+console.log(juntos)
+
+nombre += apellido;
+console.log(nombre);
+
+
