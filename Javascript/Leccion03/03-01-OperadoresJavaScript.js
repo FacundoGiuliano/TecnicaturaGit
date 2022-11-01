77c5d3b11dca0993c390fbd88ee5dbcad0cb2014
Facundo Giuliano <facugiuliano10@gmail.com>
2022-11-01 15:11:56 -0300

    Clase-7-js
    Similitudes y ejercicios js

---
+++ Javascript/Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,20 @@
+// Ejercicio para encontrar numeros pares e impares
+
+let parImpar = 10;
+if(parImpar % 2 == 0) {
+    console.log("Es un numero PAR");
+}
+else {
+    console.log("Es un numero IMPAR");
+}
+
+// Ejercicio: es mayor de edad
+
+let edad = 14, adulto = 18;
+if( edad >= adulto ) {
+    console.log("Es MAYOR de edad")
+}
+else {
+    console.log("Es MENOR de edad")
+}
+
--- Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
+++ Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -13,4 +13,29 @@ console.log(juntos)
 nombre += apellido;
 console.log(nombre);
 
+// Hoy ya no se usa var, se utiliza let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lopez"; // una constante no puede ser modificada
+console.log(apellido2)
+
+let x, y;
+
+x = 17, y = 21; // Se pueden asignar varias variables en una sola linea
+
+let z = x + y; // Se asigna el valor de una operacion
+console.log(z);
+
+let _1num = 31; // No utilizar numeros para el comienzo del nombre de la variable
+let rompiendo = "rompe" // Tampoco nmbre de palabras reservadas
+
+console.log(_1num)
+console.log(rompiendo)
+
+
+
+
+
+
 
