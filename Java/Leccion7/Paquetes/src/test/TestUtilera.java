d28f7fd6560b851533e36d34d683dccb0ee2f403
Facundo Giuliano <facugiuliano10@gmail.com>
2022-11-09 17:41:55 -0300

    Java clase 10
    Sobre carga de constructores y metodos. Palabra final

---
+++ Java/Leccion7/Paquetes/src/test/TestUtilera.java
@@ -0,0 +1,14 @@
+
+package test;
+
+//import ar.com.codesytem.*;
+import ar.com.codesytem.Utileria;
+//import static ar.com.codesystem.Utileria.imprimir; // Solo aplica para metodos estaticos
+
+public class TestUtilera {
+    public static void main(String[] args) {
+        Utileria.imprimir("Saludos a todos");
+        //imprimir("Terminamos en unos minutos");
+        ar.com.codesytem.Utileria.imprimir("Ahora si estamos terminando");
+    }
+}
