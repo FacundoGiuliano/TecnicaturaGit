ab734e7c8d2366c2ad764311274804210e5f7f2d
Facundo Giuliano <facugiuliano10@gmail.com>
2022-11-22 11:08:10 -0300

    Python clase 14
    Variables de clase, metodos estaticos, metodos de clase, contexto estatico y dinamico, constantes

---
+++ Python/Leccion10/test_constantes.py
@@ -0,0 +1,9 @@
+from Constantes import *
+from Constantes import Matematicas as Mate
+
+print(MI_CONSTANTE)
+#MI_CONSTANTE = "Aqui esta el nuevo valor  para la constante"
+#print(MI_CONSTANTE) esto no se debe hacer
+
+print(Mate)
+
